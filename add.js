33fafd77384e18ddea64d3c7ded75171fcaa6f46
shery <sherypagal123@gmail.com>
2024-02-22 03:03:35 +0500

    add two files

---
+++ add.js
@@ -0,0 +1,122 @@
+// Chapters no:1
+// Q no:1
+
+var firstName ="Sheharyar Hussain"; 
+var email = "sheharyarhussin123@gmail.com";
+var numbar = "033-21098790"
+var password = "*********"
+alert(" hello i am :"+" " +firstName+" "+" \n my email :"+" "+email+" \n my contact number:" +" " + numbar + "\n My Password Is :"+" "+ password ) ;
+
+// Q no:2
+alert("Error! Please enter a valid password");
+
+alert ("I am learning JavaScript "); 
+
+// // Q no:3
+alert("Welcom to JS Land.. \n Happy Coding");
+
+alert ("In this lesson, you will use JavaScript to display an alert after the web page has loaded.")
+
+// // Q no:4
+
+alert("Wellcom to JS Land..");
+alert("Prevent this page from creating additional dialogs.");
+
+// // Q no5
+
+alert("Hello... i can run js through my web browser's console");
+
+
+// // Q6 no:6
+
+
+// // Q no:7
+
+
+// // Chapters no:2
+// // Q no:1
+
+var username ="Jhone Don";
+alert ("username:"+" "+username);
+
+var myVariableName = "example";
+alert (myVariableName);
+
+// // Q no:2
+var fullName ="Sheharyar Hussain";
+alert ("Full Name:"+" "+ fullName);
+
+// // Q no:3
+alert("15 Years old");
+
+
+// // Q no:4
+alert("certified Mobile Application Development");
+
+// // Q no:5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// // Q no:6
+var email = "example@exaple.com";
+alert("My email address is" + email);
+
+// // Q no:7
+var book ="A smarter wayn to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Q no:8
+document.write ("Yah! I can write HTML content through JavaScript");
+
+// // Q no:9
+var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(myString);
+
+// // chaptar no3
+// // Q no1
+
+alert ("i am 15 years old")
+
+// // Q no:2
+alert ("You have visited this site 14 times")
+ 
+// // Q3 no:3
+document.write ("My birth year is 1990 <br>","Data type of my ddeclared variable is number")
+
+// // Q no:4
+var Yah = "(s)"
+ document.write("<br> <b> John Doe </b> " ," ", "ordered" , " " , "<b> 5 T-Shirt </b>" , Yah , " " ,  " on XYZ Clothing store " )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
